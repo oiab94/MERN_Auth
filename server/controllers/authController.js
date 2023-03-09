faef7d5ce141a42e3ba1818ad47cf488221a5a9d
oiab94AsusLaptop <oscar.alonso.994@gmail.com>
2023-03-08 23:17:00 -0300

    [controllers] Manejo de errores

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,26 @@
 const User = require("../models/user.model");
 
+// ! Handle Error
+const handleErrors = ({errors, message, code}) => {
+	const error =  {email:"", password:""};
+
+	// Duplicated email
+	if(code === 11000){
+		error.email = "That email is registered";
+		return error;
+	}
+
+	// Validation errors
+	if(message.includes("user validation failed")){
+		Object.values(errors).forEach(({properties}) => {
+			error[properties.path] = properties.message;
+		})
+	}
+
+	return error;
+}
+
+// * Controllers 
 module.exports.signup_get = (req, res) => {
 	res.json("signup_get")
 }
@@ -13,7 +34,10 @@ module.exports.signup_post = (req, res) => {
 
 	User.create({email, password})
 		.then(result => res.status(201).json(result))
-		.catch(error => res.status(400).json(error));
+		.catch(err => {
+			const errors = handleErrors(err);			
+			res.status(400).json(errors);
+		});
 }
 
 module.exports.login_post = (req, res) => {
