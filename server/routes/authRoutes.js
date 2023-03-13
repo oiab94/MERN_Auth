cf0fc493bd48f0602f9ca8a9c1553edb5ec2877e
oiab94AsusLaptop <oscar.alonso.994@gmail.com>
2023-03-13 13:02:34 -0300

    [Routes] Cambio de direccion

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -9,6 +9,6 @@ router.post("/signup", authController.signup_post);
 router.get("/login", authController.signup_get);
 router.post("/login", authController.login_post);
 router.get("/logout", authController.logout_get);
-router.get("/authPage", requireAuth);
+router.get("/authUser", requireAuth);
 
 module.exports = router;
