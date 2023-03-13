e523af512018b86f26d069927a7020d00029a40e
oiab94AsusLaptop <oscar.alonso.994@gmail.com>
2023-03-12 21:02:43 -0300

    [Logout] Controla la forma de hacer logout

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -81,3 +81,8 @@ module.exports.login_post = (req, res) => {
 			res.status(400).json(errors);
 		})
 }
+
+module.exports.logout_get = (req, res) => {
+	res.cookie("login", "", {maxAge: 1});
+	res.status(200).json({ message:"User logout" });
+}
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -8,6 +8,7 @@ router.get("/signup", authController.signup_get);
 router.post("/signup", authController.signup_post);
 router.get("/login", authController.signup_get);
 router.post("/login", authController.login_post);
-router.get("/authPage", requireAuth)
+router.get("/logout", authController.logout_get);
+router.get("/authPage", requireAuth);
 
 module.exports = router;
